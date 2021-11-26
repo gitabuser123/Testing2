a0e0759016b3f427875c161d2456f27195631a23
chillpert <dev@christianhilpert.com>
2021-11-26 11:28:39 +0100

    Initial commit

---
+++ Source/Testing2/Testing2GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Testing2GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTING2_API ATesting2GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
